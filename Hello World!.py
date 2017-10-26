fb37d4ea81692e2a30e3159dd82b84aa6e5e3b60
FreedomSka <edoardo.zerbo@gmail.com>
2017-10-26 02:17:39 +0200

    added Hello World!
    Obfuscate Hello World 0.1

---
+++ Hello World!.py
@@ -0,0 +1,4 @@
+#!/usr/bin env python
+getattr(__import__(().__class__.__name__[2] + ().__class__.__name__[2] + ''.join(chr((341881320659697397625452>>8*(12-byte-1))&0xFF) for byte in range(12)).__class__.__name__[2] + int(13).__class__.__name__),
+	    ().__class__.__name__[2] + ().__class__.__name__[2] + ''.join(chr((341881320659697397625452>>8*(12-byte-1))&0xFF) for byte in range(12)).__class__.__name__[2] + int(13).__class__.__name__)(
+	    ''.join([chr(int(''.join(c), 16)) for c in zip('48656c6c6f20576f726c6421'[0::2],'48656c6c6f20576f726c6421'[1::2])]))
