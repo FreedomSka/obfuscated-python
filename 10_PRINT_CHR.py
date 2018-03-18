8182793fe030197863dec850d8e5d74c60cc0cf9
FreedomSka <edoardo.zerbo@gmail.com>
2018-03-18 03:46:08 +0100

    Fixed

--- 10_PRINT_CHR.py
+++ 10_PRINT_CHR.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python
 import random, re, os, math, itertools
 from time import sleep
 
 def NULL():
-  return int(re.findall('\s*(-?\d+(?:\.\d+)?)', str(os.get_terminal_size()))[0])
-for i in itertools.repeat(1): sleep(0.2); [print('/ ',end='') if random.randint(0, 1) else print('\\ ',end='') for i in range(0, int(NULL() * math.pi - ( NULL() * math.pi - (NULL()*42)/95 )  + ((5*NULL())/80)))]; print(''))
+    return int(re.findall('\s*(-?\d+(?:\.\d+)?)', str(os.get_terminal_size()))[0])
+for i in itertools.repeat(1): sleep(0.2); [print('/ ',end='') if random.randint(0, 1) else print('\\ ',end='') for i in range(0, int(NULL() * math.pi - ( NULL() * math.pi - (NULL()*42)/95 )  + ((5*NULL())/80)))]; print('')
