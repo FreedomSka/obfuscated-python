1c9931be4893da053a5cf50949b9405a050b322a
FreedomSka <edoardo.zerbo@gmail.com>
2018-03-21 22:46:20 +0100

    Update 10_PRINT_CHR.py

--- 10_PRINT_CHR.py
+++ 10_PRINT_CHR.py
@@ -1,6 +1,7 @@
-import random, re, os, math, itertools
-from time import sleep
+#!/usr/bin/env python                                             
+import random, re, os, math, itertools                            
+from time import sleep                                            
 
-def NULL():
-    return int(re.findall('\s*(-?\d+(?:\.\d+)?)', str(os.get_terminal_size()))[0])
-for i in itertools.repeat(1): sleep(0.2); [print('/ ',end='') if random.randint(0, 1) else print('\\ ',end='') for i in range(0, int(NULL() * math.pi - ( NULL() * math.pi - (NULL()*42)/95 )  + ((5*NULL())/80)))]; print('')
+def NULL():                                                                            
+  return int(re.findall('\s*(-?\d+(?:\.\d+)?)', str(os.get_terminal_size()))[0])     
+for i in itertools.repeat(1): sleep(0.2); [print(chr(8944 + random.randint(0,1)) + ' ', end='') for i in range(0, int(NULL() * math.pi - ( NULL() * math.pi - (NULL()*42)/95 )  + ((5*NULL())/80)))]; print('')
